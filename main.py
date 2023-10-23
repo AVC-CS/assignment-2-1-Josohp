170e9fd65f7026feabcf40dcd1f55d19466249b6
Josohp <jadistan4@gmail.com>
2023-10-23 08:52:34 -0700

    done

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    mnum = int(input('enter number of male students: '))
+    fnum = int(input('enter number of female students: '))
+    total = mnum + fnum
+    m_perc = mnum / total * 100
+    f_perc = fnum / total * 100
+    print (f'the percantage of male students is {m_perc:.2f}')
+    print (f'the percantage of female students is {f_perc:.2f}')
+
+    
 
     """
     ########################################
